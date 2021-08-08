9ef2e8e8c16fd90c43e1a6af584bf944578ede23
Dovakiin0 <subedibs@outlook.com>
2021-08-08 22:08:18 +0545

    recent card

--- app/components/Cards.js
+++ app/components/Cards.js
@@ -2,23 +2,28 @@ import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { useTheme } from "@react-navigation/native";
 
-const Cards = () => {
+const Cards = ({ img, title, extra }) => {
   const { colors } = useTheme();
   return (
     <View style={styles.container}>
       {/* Image */}
-      <View>
+      <View style={styles.imgWrapper}>
         <Image
           style={styles.coverImg}
           source={{
-            uri: "https://picsum.photos/200/300",
+            uri: img,
           }}
           resizeMode="cover"
         />
+        <View style={styles.extra}>
+          <Text style={[{ color: colors.text }, styles.extraText]}>
+            {extra}
+          </Text>
+        </View>
       </View>
       {/* title */}
-      <View>
-        <Text style={{ color: colors.text }}>Title</Text>
+      <View style={styles.titleWrpper}>
+        <Text style={[styles.title, { color: colors.text }]}>{title}</Text>
       </View>
     </View>
   );
@@ -31,8 +36,34 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 5,
   },
+  imgWrapper: {
+    justifyContent: "center",
+  },
   coverImg: {
-    width: 150,
-    height: 200,
+    width: 200,
+    height: 250,
+    borderRadius: 5,
+  },
+  title: {
+    fontWeight: 15,
+    marginTop: 10,
+    fontWeight: "bold",
+    flexWrap: "wrap",
+  },
+  titleWrpper: {
+    flex: 1,
+  },
+  extra: {
+    backgroundColor: "#aa647b",
+    width: "100%",
+    height: 25,
+    position: "absolute",
+    bottom: 0,
+    overflow: "hidden",
+    borderRadius: 5,
+    justifyContent: "center",
+  },
+  extraText: {
+    fontWeight: "bold",
   },
 });
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import Cards from "../components/Cards";
+import axios from "axios";
 
 const HomeScreen = () => {
   const { colors } = useTheme();
   const [recent, setrecent] = useState([]);
 
-  const getRecent = async () => {};
+  const getRecent = async () => {
+    axios
+      .get("http://localhost:3030/api/v1/anime/recent/1")
+      .then((res) => {
+        setrecent(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     getRecent();
@@ -17,9 +25,15 @@ const HomeScreen = () => {
     <View>
       <Text style={[styles.title, { color: colors.text }]}>Recent Release</Text>
       <View style={styles.cardContainer}>
-        <Cards />
-        <Cards />
-        <Cards />
+        {recent.length !== 0
+          ? recent.map((anime, i) => (
+              <Cards
+                title={anime.name}
+                img={anime.img}
+                extra={anime.recent_episode}
+              />
+            ))
+          : ""}
         <Cards />
       </View>
 
